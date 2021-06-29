9c5550bb8424842ba17c49efcefdb6d3a8e01648
oukouwh <oukouwh@163.com>
2021-06-29 16:59:37 +0900

    测试用

---
+++ demo/Helloworld.java
@@ -0,0 +1,14 @@
+/*
+ * @Author: wang hao
+ * @Date: 2021-06-29 16:54:49
+ * @LastEditTime: 2021-06-29 16:58:39
+ * @LastEditors: Please set LastEditors
+ * @Description: java之helloworld
+ * @FilePath: /java_study/demo/Helloworld.java
+ */
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
